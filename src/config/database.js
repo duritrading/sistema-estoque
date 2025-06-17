9f102c22f733ff5cbd5644f4da28372464fa1254
duritrading <marketing@duri.com.br>
2025-06-17 20:56:48 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,10 @@
-
+// Auto-detectar qual banco usar
+if (process.env.DATABASE_URL) {
+  // PostgreSQL em produção
+  console.log('🐘 Usando PostgreSQL (produção)');
+  module.exports = require('./database-postgres');
+} else {
+  // SQLite em desenvolvimento
+  console.log('🗄️ Usando SQLite (desenvolvimento)');
+  module.exports = require('./database-sqlite');
+}
